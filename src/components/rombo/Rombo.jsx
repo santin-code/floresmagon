315d46494573b52d70368b8905dfcf0e54272ab8
ArkalKeirosRas <recargamarcas6@gmail.com>
2023-08-15 00:55:00 -0600

    oo

---
+++ src/components/rombo/Rombo.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./rombo.css";
+import { GiRocket } from "react-icons/gi";
+
+const Rombo = () => {
+  return (
+    <>
+      <div className="rombo_principal_container">
+        <div>
+          <div className="rombo_container">
+            <GiRocket className="rocket" />
+            <div class="rombo2">actas de nacimiento certificadas</div>
+          </div>
+
+          <div className="rombo_container">
+            <GiRocket className="rocket" />
+            <div class="rombo2">antecedentes no penales</div>
+          </div>
+
+          <div className="rombo_container">
+            <GiRocket className="rocket" />
+            <div class="rombo2">numero de seguro social</div>
+          </div>
+
+          <div className="rombo_container">
+            <GiRocket className="rocket" />
+            <div class="rombo2">vigencia de derechos IMSS</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Rombo;
