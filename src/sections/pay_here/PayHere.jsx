580fa6b651010a57f50abef23b42ee3988b73061
ArkalKeirosRas <recargamarcas6@gmail.com>
2023-08-14 20:42:59 -0600

    ll

--- src/sections/pay_here/PayHere.jsx
+++ src/sections/pay_here/PayHere.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./pay_here.css";
 import CFE from "../../images/cfelogo.webp";
 import MEGACABLE from '../../images/megacable.svg';
-import BAIT from "../../images/bait.png";
+import BAIT from '../../images/bait.png';
+import TELCEL from '../../images/TELCEL.webp';
+import ATT from '../../images/ATT.webp';
+import MOVISTAR from '../../images/movistar.png';
+import UNEFON from '../../images/UNEFON.png';
+
 
 const PayHere = () => {
   return (
@@ -22,6 +27,12 @@ const PayHere = () => {
         <div>
           <p className="reload">recarga</p>
           <img src={BAIT} alt="logo_bait" className="logo_bait min_opacity" />
+          <div className="other_companies">
+            <img src={TELCEL} alt="logo_telcel" className="companies" />
+            <img src={ATT} alt="logo_att" className="companies" />
+            <img src={MOVISTAR} alt="logo_movistar" className="companies" />
+            <img src={UNEFON} alt="logo_unefon" className="companies" />
+          </div>
         </div>
       </div>
     </>
